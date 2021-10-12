3bed84912d9f2306d7a89edd96eb3b81ba08e945
Igor Yagushevsky <yagushevskij@gmail.com>
2021-10-12 17:14:48 +0300

    fetch data from server

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 import AppHeader from '../app-header/app-header.js';
 import BurgerIngridients from '../burger-ingredients/burger-ingredients.js';
 import BurgerConstructor from '../burger-constructor/burger-constructor.js';
 import app from './app.module.css';
-import { data } from '../../utils/data.js';
-import { basket } from '../../utils/data.js';
 
 const App = () => {
+  const [state, setState] = useState({ data: [] });
+  useEffect(() => {
+    getIngredientsData();
+  }, []);
+
+  const getIngredientsData = async () => {
+    try {
+      setState({ ...state, loading: true });
+      const res = await fetch(`https://norma.nomoreparties.space/api/ingredients `);
+      const resData = await res.json();
+      setState({
+        ...state,
+        data: resData.data, loading: false
+      });
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
     <>
       <AppHeader />
       <main className={app.main}>
-        <BurgerIngridients data={data} />
-        <BurgerConstructor data={basket} />
+        <BurgerIngridients data={state.data} />
+        <BurgerConstructor data={state.data} />
       </main>
     </>
   );
