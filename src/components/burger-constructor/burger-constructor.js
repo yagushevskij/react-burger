0746e45653159bd4fdd8cde03ac6c68c933d5899
Igor Yagushevsky <yagushevskij@gmail.com>
2021-10-20 14:10:07 +0300

    added total cost counting

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useReducer, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import ingridientPropTypes from '../../utils/type'
@@ -10,26 +10,48 @@ import Modal from '../modal/modal';
 import OrderDetails from '../order-details/order-details';
 import { BasketContext } from '../../services/basket-context';
 
+const basketInitialState = { total: 0 };
+
+const basketReducer = (state, action) => {
+  switch (action.type) {
+    case 'add':
+      return { total: state.total + action.cost };
+    case 'remove':
+      return { total: state.total + action.cost };
+    default:
+      throw new Error(`Wrong type of action: ${action.type}`);
+  }
+}
+
 const BurgerConstructor = () => {
   const { basket } = useContext(BasketContext);
-  const [state, setState] = useState({
+  const [modal, setModal] = useState({
     isModalOpened: false
   });
+  const [totalCost, totalCostDispatcher] = useReducer(basketReducer, basketInitialState);
+  useEffect(() => {
+    basket.forEach((el) => {
+      totalCostDispatcher({
+        type: 'add',
+        cost: el.price
+      })
+    })
+  }, [basket])
 
   const handleCloseModal = () => {
-    setState({
-      ...state,
+    setModal({
+      ...modal,
       isModalOpened: false
     })
   }
   const handleOpenModal = () => {
-    setState({
-      ...state,
+    setModal({
+      ...modal,
       isModalOpened: true,
     })
   }
 
-  const modal = (
+  const modalComp = (
     <Modal onClose={handleCloseModal}>
       <OrderDetails />
     </Modal>
@@ -38,59 +60,59 @@ const BurgerConstructor = () => {
   const ingredientsWithoutBun = basket.filter(el => el.type !== 'bun');
   return (
     <>
-        {state.isModalOpened && modal}
-        <section className={`${burgerConstructor.section} ml-10 pl-4 mt-25`}>
-          <ul className={`${burgerConstructor.list}`}>
+      {modal.isModalOpened && modalComp}
+      <section className={`${burgerConstructor.section} ml-10 pl-4 mt-25`}>
+        <ul className={`${burgerConstructor.list}`}>
+          {
+            bun &&
+            <li className='ml-8' key={bun._id}>
+              <ConstructorElement
+                type='top'
+                isLocked={true}
+                text={`${bun.name} (верх)`}
+                price={bun.price}
+                thumbnail={bun.image}
+              />
+            </li>
+          }
+          <ul className={`${burgerConstructor.list} ${burgerConstructor.scroll}`}>
             {
-              bun &&
-              <li className='ml-8' key={bun._id}>
-                <ConstructorElement
-                  type='top'
-                  isLocked={true}
-                  text={`${bun.name} (верх)`}
-                  price={bun.price}
-                  thumbnail={bun.image}
-                />
-              </li>
-            }
-            <ul className={`${burgerConstructor.list} ${burgerConstructor.scroll}`}>
-              {
-                ingredientsWithoutBun &&
-                ingredientsWithoutBun.map((item, index) =>
-                  <li className={`${burgerConstructor.item}`} key={index}>
-                    <DragIcon type='primary' />
-                    <ConstructorElement
-                      text={item.name}
-                      price={item.price}
-                      thumbnail={item.image}
-                    />
-                  </li>
-                )
-              }
-            </ul>
-            {
-              bun &&
-              <li className='ml-8' key={bun._id}>
-                <ConstructorElement
-                  type='bottom'
-                  isLocked={true}
-                  text={`${bun.name} (низ)`}
-                  price={bun.price}
-                  thumbnail={bun.image}
-                />
-              </li>
+              ingredientsWithoutBun &&
+              ingredientsWithoutBun.map((item, index) =>
+                <li className={`${burgerConstructor.item}`} key={index}>
+                  <DragIcon type='primary' />
+                  <ConstructorElement
+                    text={item.name}
+                    price={item.price}
+                    thumbnail={item.image}
+                  />
+                </li>
+              )
             }
           </ul>
-          <div className={`${burgerConstructor.total} mt-10`}>
-            <p className={`${burgerConstructor.total__cost} mr-10`}>
-              <span className='text text_type_digits-medium mr-2'>610</span>
-              <CurrencyIcon type='primary' />
-            </p>
-            <Button type='primary' size='large' onClick={handleOpenModal}>
-              Оформить заказ
-            </Button>
-          </div>
-        </section>
+          {
+            bun &&
+            <li className='ml-8' key={bun._id}>
+              <ConstructorElement
+                type='bottom'
+                isLocked={true}
+                text={`${bun.name} (низ)`}
+                price={bun.price}
+                thumbnail={bun.image}
+              />
+            </li>
+          }
+        </ul>
+        <div className={`${burgerConstructor.total} mt-10`}>
+          <p className={`${burgerConstructor.total__cost} mr-10`}>
+            <span className='text text_type_digits-medium mr-2'>{totalCost.total}</span>
+            <CurrencyIcon type='primary' />
+          </p>
+          <Button type='primary' size='large' onClick={handleOpenModal}>
+            Оформить заказ
+          </Button>
+        </div>
+      </section>
     </>
   );
 }
