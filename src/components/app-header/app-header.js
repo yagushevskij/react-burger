524972d82294f35f57cad3567c3a221fafdbbd6a
Igor Yagushevsky <yagushevskij@gmail.com>
2021-11-19 14:34:07 +0300

    fixed header activelink bug

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,31 +3,33 @@ import appHeader from './app-header.module.css'
 import { Logo, ProfileIcon, BurgerIcon, ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import { NavLink } from 'react-router-dom'
 import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { DATA } from '../../utils/config'
+
+const links = DATA.header.links
+const primaryIcon = 'primary'
+const secondaryIcon = 'secondary'
 
 const AppHeader = () => {
-  const [activeIcon, setActiveIcon] = useState()
-  const setLinkStyle = (isActive, link) => {
-    if (isActive) {
-      setActiveIcon(link)
-    }
-    return `${appHeader.link} text text_type_main-default text_color_inactive ml-2 ${isActive && appHeader.link_active}`
-  }
-  const primaryIcon = 'primary'
-  const secondaryIcon = 'secondary'
+  const location = useLocation()
+  console.log(location.pathname)
+  // const [activeIcon, setActiveIcon] = useState()
+  const setLinkStyle = ({isActive}) => `${appHeader.link} text text_type_main-default text_color_inactive ml-2 ${isActive && appHeader.link_active}`
+  const setActiveIcon = (path) => location.pathname === path ? primaryIcon : secondaryIcon
   return (
     <header className={appHeader.header}>
       <div className={appHeader.header__wrapper}>
         <ul className={`${appHeader.item} ${appHeader.item_pos_left}`}>
           <li className={`${appHeader.item__link} p-5`}>
-            <BurgerIcon type={activeIcon === 'constructor' ? primaryIcon : secondaryIcon} />
+            <BurgerIcon type={setActiveIcon(links.constructor.path)} />
             <NavLink end to={`/`} className={({ isActive }) => setLinkStyle(isActive, 'constructor')}>
-              Конструктор
+              {links.constructor.title}
             </NavLink>
           </li>
           <li className={`${appHeader.item__link} p-5 ml-2`}>
-            <ListIcon type={activeIcon === 'orders' ? primaryIcon : secondaryIcon} />
+            <ListIcon type={setActiveIcon(links.ordersFeed.path)} />
             <NavLink end to={`/profile/orders`} className={({ isActive }) => setLinkStyle(isActive, 'orders')}>
-              Лента заказов
+            {links.ordersFeed.title}
             </NavLink>
           </li>
         </ul>
@@ -36,9 +38,9 @@ const AppHeader = () => {
         </div>
         <ul className={`${appHeader.item} ${appHeader.item_pos_right}`}>
           <li className={`${appHeader.item__link} p-5`}>
-            <ProfileIcon type={activeIcon === 'profile' ? primaryIcon : secondaryIcon} />
-            <NavLink end to={`/profile`} className={({ isActive }) => setLinkStyle(isActive, 'profile')}>
-              Личный кабинет
+            <ProfileIcon type={setActiveIcon(links.profile.path)} />
+            <NavLink end to={`/profile`} className={setLinkStyle}>
+            {links.profile.title}
             </NavLink>
           </li>
         </ul>
