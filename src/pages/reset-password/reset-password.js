678b7673674e985c8af7d8249e8d02c1f74d854d
Igor Yagushevsky <yagushevskij@gmail.com>
2021-11-19 17:47:07 +0300

    protect reset pass page from direct access

--- src/pages/reset-password/reset-password.js
+++ src/pages/reset-password/reset-password.js
@@ -1,14 +1,19 @@
 import styles from './reset-password.module.css'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { Navigate } from 'react-router-dom'
+import { Navigate, useLocation } from 'react-router-dom'
 import ResetPasswordForm from '../../components/reset-password-form/reset-password-form'
 
 const ResetPassword = () => {
+  const location = useLocation()
+  const state = location.state
   const isRequestSuccess = useSelector(state => state.resetPass.success)
 
   if (isRequestSuccess) {
     return <Navigate replace to={'/'} />
+  }
+  if (state?.from.pathname !== '/forgot-password') {
+    return <Navigate replace to={'/forgot-password'} />
   }
 
   return (
